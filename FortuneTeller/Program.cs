25111e9f933d286b9dcfd49af5c208aabce14d3b
rhorrocks <tipphorrocks@msn.com>
2016-07-08 19:36:07 -0400

    first commit

---
+++ FortuneTeller/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FortuneTeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Fortune Teller Program
+            Console.WriteLine("Welcome to the Fortune Teller Program");
+
+        }
+    }
+}

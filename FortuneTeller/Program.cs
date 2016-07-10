5b460b7a70a69a5a0324edfff7b3c1927ab9687b
rhorrocks <tipphorrocks@msn.com>
2016-07-09 21:19:19 -0400

    adding part 2 bank balance code

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -25,29 +25,82 @@ namespace FortuneTeller
             Console.WriteLine("Please enter your LAST name and hit \"Enter\"");
             string userLastName = Console.ReadLine();
 
+            //combining the first and last name into a concatenated string for the first/second/third letter of birth month test
+            string userFullName = string.Concat(userFirstName, userLastName).ToUpper();
+
+
             Console.WriteLine("Please enter your AGE and hit \"Enter\"");
             int userAge = int.Parse(Console.ReadLine());
+
             //if the user's age is even, they will retire in 17 years
             //if the user's age is odd, they will retire in 6 years
+
             userAge = (userAge % 2 == 0) ? 17 : 6;
-            
-            
+
+
             Console.WriteLine("Please enter your BIRTH MONTH and hit \"Enter\"");
-            string userBirthMonth = Console.ReadLine();
+            string userBirthMonth = Console.ReadLine().ToUpper();
+
+            //Getting substrings of each of the first three letters in the birth month for the bank balance
+
+            string firstLetterOfBirthMonth = userBirthMonth.Substring(0, 1);
+            string secondLetterOfBirthMonth = userBirthMonth.Substring(1, 1);
+            string thirdLetterOfBirthMonth = userBirthMonth.Substring(2, 1);
+
+            int indexFirstLetter = userFullName.IndexOf(firstLetterOfBirthMonth);
+            //Console.WriteLine(indexFirstLetter);
+            int indexSecondLetter = userFullName.IndexOf(secondLetterOfBirthMonth);
+            //Console.WriteLine(indexSecondLetter);
+            int indexThirdLetter = userFullName.IndexOf(thirdLetterOfBirthMonth);
+            //Console.WriteLine(indexThirdLetter);
 
 
 
-            Console.WriteLine("What is your favorite ROYGBIV color? If you do not know what ROYGBIV is, enter \"Help\" to get a list of the ROYGBIV colors.");
+            double moneyInBank = 25000.00;
+
+            if(indexFirstLetter != -1)
+            {
+                moneyInBank = 8500000.12;
+            }
+            else if(indexSecondLetter != -1)
+            {
+                moneyInBank = 26500000.67;
+            }
+            else if(indexThirdLetter != -1)
+            {
+                moneyInBank = 978999.54;
+            }
+            else 
+            {
+                moneyInBank = 35000.00;
+            }
+            
+            //Console.WriteLine(firstLetterOfBirthMonth + secondLetterOfBirthMonth + thirdLetterOfBirthMonth);
+
+            Console.WriteLine("What is the first letter of your favorite ROYGBIV color? If you do not know what ROYGBIV is, enter \"Help\" to get a list of the ROYGBIV colors.");
             string roygbivValue = Console.ReadLine();
-            roygbivValue = roygbivValue.ToUpper(); //changing user entered cast to upper so we could run switch case
-            if(roygbivValue == "HELP")
+            roygbivValue = roygbivValue.ToUpper(); 
+            
+            //changing user entered cast to upper so we could run switch case
+            string modeOfTransportation = "car";
+
+            //building in the "Help" conditional
+
+            if (roygbivValue == "HELP")
             {
                 Console.WriteLine("ROYGBIV stands for \nR = Red\nO = Orange\nY = Yellow");
                 Console.WriteLine("G = Green\nB = Blue\nI = Indigo\nV = Violet");
                 Console.WriteLine("Please enter your favorite ROYGBIV color, and hit \"Enter\"");
                 roygbivValue = Convert.ToString(Console.ReadLine());
             }
-            string modeOfTransportation = "car";
+           
+
+
+            //Returning a substring and applying just in case the user puts in the whole name of the color
+            string firstLetterOfRoygbivValue = roygbivValue.Substring(0, 1);
+            roygbivValue = firstLetterOfRoygbivValue;
+
+
 
             switch (roygbivValue)
             {
@@ -112,8 +165,11 @@ namespace FortuneTeller
 
 
 
-            Console.Write(userFirstName + " " + userLastName + " will retire in " + userAge + " years,");
-            Console.Write(" a vacation home in " + userFutureLocation + " and a " + modeOfTransportation);
+            Console.Write(userFirstName + " " + userLastName + " will retire in " + userAge + " years ");
+            Console.Write("with $" + moneyInBank + " in the bank");
+            Console.Write(", a vacation home in " + userFutureLocation + " and a " + modeOfTransportation +".");
+
+
 
             
             
